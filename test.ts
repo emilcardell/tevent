6a1c25373b817bb3bd65697f864011020037194c
Emil Cardell <emil.cardell@gmail.com>
2024-03-18 23:31:07 +0100

    Not working as expected

--- test.ts
+++ test.ts
@@ -10,3 +10,31 @@ function input<$Parser extends Parser>(input: $Parser) {
 input(z.object({ objectionId: z.string(), comment: z.string() }));
 
 const what = z.object({ objectionId: z.string(), comment: z.string() });
+
+const useMessageQueue = <TValues>(
+    schema: z.Schema<TValues>
+    ) => {
+    console.log(schema);
+    return {
+        schema,
+        publisher: {
+            publish: (values: TValues) => {
+                schema.parse(values);
+                return values
+            }
+        },
+        subscriber: {
+            on: (callback: (values: TValues) => void) => { 
+
+            }
+            
+        }
+    }
+}
+
+
+//const { publisher, subscriber } = useStreamer(validationSchema);
+/*
+publisher.publish({ objectionId: "1", comment: "test" });
+subscriber('group').on((data)=> console.log(data));
+*/
--- test2.ts
+++ test2.ts
@@ -1,84 +1,144 @@
 import { z } from "zod";
 import Redis from "ioredis";
 
-const validationSchema = z.object({ objectionId: z.string(), comment: z.string() });
+const validationSchema = z.object({
+  objectionId: z.string(),
+  comment: z.string(),
+});
 const anotherValidationSchema = z.object({ id: z.string(), body: z.string() });
 
+const useStream = <TValues>(schema: z.Schema<TValues>) => {
+  return {
+    streamSchema: schema,
+  };
+};
 
-const useStreamer = <TValues>(
-    schema: z.Schema<TValues>
-    ) => {
-    return {
-        publisher: {
-            publish: (values: TValues) => {
-                schema.parse(values);
-                return values
-            }
+const useQueue = <TValues>(schema: z.Schema<TValues>) => {
+  return {
+    queueSchema: schema,
+  };
+};
+
+const createPublisher = <TValues>(schema: z.Schema<TValues>) => {
+  return {
+    schema: schema,
+    publish: (values: TValues) => {
+      schema.parse(values);
+      return values;
+    },
+  };
+};
+const createSubscriber = <TValues>(schema: z.Schema<TValues>) => {
+  return {
+    schema: schema,
+    on: (callback: (values: TValues) => void) => {
+      return {
+        subscribe() {
+          return callback;
         },
-        subscriber(group: string): {
-            on: (callback: (values: TValues) => void) => { 
+      };
+    },
+  };
+};
 
-            }
-        }
-    }
+export type publisherType = ReturnType<typeof createPublisher>;
+export type subscriberType = ReturnType<typeof createSubscriber>;
+
+export interface PublisherRecord<TValues> {
+  [key: string]: ReturnType<typeof createPublisher<TValues>>;
+}
+export interface SubscriberRecord {
+  [key: string]: subscriberType;
 }
 
-export type useStreamerType = ReturnType<typeof useStreamer>;
+interface Publisher<TValues> {
+  schema: z.Schema<TValues>;
+  publish: (values: TValues) => TValues;
+}
+
+export type useStreamType = ReturnType<typeof useStream>;
+export type useQueueType = ReturnType<typeof useQueue>;
+function isStream(
+  useType: useStreamType | useQueueType
+): useType is useStreamType {
+  return useType.hasOwnProperty("streamSchema");
+}
+function isQueue(
+  useType: useStreamType | useQueueType
+): useType is useStreamType {
+  return useType.hasOwnProperty("queueSchema");
+}
 
 export interface ProcedureRouterRecord {
-    [key: string]: useStreamerType;
-  }
-
-const useMessageQueue = <TValues>(
-    schema: z.Schema<TValues>
-    ) => {
-    console.log(schema);
-    return {
-        schema,
-        publisher: {
-            publish: (values: TValues) => {
-                schema.parse(values);
-                return values
-            }
-        },
-        subscriber: {
-            on: (callback: (values: TValues) => void) => { 
-
-            }
-            
-        }
-    }
+  [key: string]: useStreamType | useQueueType;
 }
 
-
-
-const { publisher, subscriber } = useStreamer(validationSchema);
-
-publisher.publish({ objectionId: "1", comment: "test" });
-subscriber('group').on((data)=> console.log(data));
-
-const createStreamContext = <TValues>(streams: TValues) => { 
-    return streams;
-}
-
+const createStreamContext = <TValues extends ProcedureRouterRecord>(
+  streams: TValues
+) => {
+  return streams;
+};
 
 const allTheThings = createStreamContext({
-    something: useStreamer(validationSchema),
-    somethingElse: useStreamer(anotherValidationSchema)
-})
-
+  something: useStream(validationSchema),
+  somethingElse: useQueue(anotherValidationSchema),
+});
 
 export type EventRouter = typeof allTheThings;
 
+const redisStream = <TValue extends ProcedureRouterRecord>(
+  redisConnection: Redis,
+  streams: TValue
+) => {
+  const internalPublishers = {} as PublisherRecord<any>;
+  const externalPublishers = {} as PublisherRecord<any>;
+  const internalSubscribers = {} as SubscriberRecord;
+  const externalSubScribers = {} as SubscriberRecord;
+  
+  Object.entries(streams)
+      .forEach(([key, value]) => console.log(`${key}: ${value}`))
 
-createStreamContext({
-    something: useStreamer(validationSchema),
-    somethingElse: useStreamer(anotherValidationSchema)
-})
 
-const redisStream = <TValues extends ProcedureRouterRecord>(redisConnection: Redis, streams: TValues extends ProcedureRouterRecord) => {
+  Object.keys(streams).forEach((key) => {
+    const prop = streams[key];
+    if (isStream(prop)) {
+      type schemaType = z.infer<typeof prop.streamSchema>;
+      internalPublishers[key] = createPublisher<schemaType>(prop.streamSchema);
+      externalSubScribers[key] = createSubscriber(prop.streamSchema);
+    }
 
-    Object.keys(streams).forEach(key => {
+    if (isQueue(prop)) {
+      type schemaType = z.infer<typeof prop.streamSchema>;
+      internalSubscribers[key] = createSubscriber<schemaType>(
+        prop.streamSchema
+      );
+      externalPublishers[key] = createPublisher<schemaType>(prop.streamSchema);
+    }
+  });
+
+  return {
+    internalPublishers,
+    externalPublishers,
+    internalSubscribers,
+    externalSubScribers,
+  };
+};
+const redis = new Redis();
+const { externalPublishers, externalSubScribers } = redisStream<EventRouter>(
+  redis,
+  allTheThings
+);
+externalPublishers.something.publish({ id: "1", comment: "test" });
+externalSubScribers.something.on((data) => console.log(data.id));
+
+/*
+  
+ Object.keys(streams).forEach(key => {
+        if(streams[key].streamSchema) {
+            internal[key] = streams[key].streamSchema;
+        }
+
+
         streams[key].publisher = {
             publish: (values: TValues) => {
                 streams[key].parse(values);
@@ -108,14 +168,5 @@ const redisStream = <TValues extends ProcedureRouterRecord>(redisConnection: Red
         }
     });
 
-    return streams;
-}
-const redis = new Redis();
-const streams = redisStream(redis, allTheThings);
-streams.something.publisher.publish({ objectionId: "1", comment: "test" });
-streams.something.subscriber('group').on((data)=> console.log(data));
 
-
-
-
-
+     */
